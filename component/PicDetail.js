ae997ce399b153e569f2fbdd78a1dec46851bd78
greatzues <greatzly@outlook.com>
2017-02-14 14:43:21 +0800

    完成评论功能

--- component/PicDetail.js
+++ component/PicDetail.js
@@ -1,27 +1,59 @@
 /**
  * Created by zues on 2016/11/17.
+ * 接受两个属性，分别是uri和图片所在数组的index
  */
 import React,{ Component } from 'react';
-import { View, Image, Navigator, Dimensions, StyleSheet } from 'react-native';
+import { View, Image, Navigator, Dimensions, StyleSheet, TouchableWithoutFeedback, Text } from 'react-native';
 import NormalToolbar from './NormalToolbar';
-import Net from '../Tool'
+import Net from '../Tool';
+import Swiper from 'react-native-swiper'
+import PhotoView from 'react-native-photo-view'
 
-var window = Dimensions.get('window');
 export default class PicDetail extends Component{
     // 构造
       constructor(props) {
         super(props);
-        // 初始状态
-        this.state = {
-            uri:this.props.uri
-        };
+          this.state={
+              testData:this.props.uri
+          }
       }
 
       render(){
+          let source = [];
+          for(x in this.state.testData){
+              this.state.testData[x]===null?null:source.push(this.state.testData[x]);
+          }
           return(
               <View style={styles.container}>
-                  <NormalToolbar leftItemFunc={this.back.bind(this)} title='图片详情' leftImageSource={require('../img/back.png')}/>
-                  <Image source={{uri:this.props.uri}} style={styles.image} resizeMode='contain'/>
+                  <NormalToolbar
+                      leftItemFunc={this.back.bind(this)}
+                      leftItemTitle='返回'
+                      leftTextColor='white'
+                      barBGColor='transparent'
+                      title='图片详情'
+                      titleTextColor='white'
+                  />
+                  <Swiper
+                      index={this.props.index-1}
+                      style={styles.wrapper}
+                      renderPagination={renderPagination}
+                      showsButtons={true}
+                      loop={false}
+                  >
+                      {
+                          source.map((item, i) => <View key={i} style={styles.slide}>
+                              <TouchableWithoutFeedback>
+                                  <PhotoView
+                                      source={{uri: BASEURL+'/message/'+item}}
+                                      resizeMode='contain'
+                                      minimumZoomScale={1}
+                                      maximumZoomScale={5}
+                                      androidScaleType='fitCenter'
+                                      style={styles.photo} />
+                              </TouchableWithoutFeedback>
+                          </View>)
+                      }
+                  </Swiper>
               </View>
           );
       }
@@ -31,14 +63,53 @@ export default class PicDetail extends Component{
     }
 }
 
+const renderPagination = (index, total, context) => {
+    return (
+        <View style={{
+            position: 'absolute',
+            justifyContent: 'center',
+            alignItems: 'center',
+            top: 15,
+            left: 0,
+            right: 0
+        }}>
+            <View style={{borderRadius: 7, backgroundColor: 'rgba(255,255,255,.15)', padding: 3, paddingHorizontal: 7}}>
+                <Text style={{color: '#fff', fontSize: 14}}>{index + 1} / {total}</Text>
+            </View>
+        </View>
+    )
+}
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'white',
+        backgroundColor: 'black',
     },
     image:{
-        width: window.width*0.9,
-        height: window.height,
-        marginLeft: window.width*0.05,
+        width: device.width*0.9,
+        height: device.height,
+        marginLeft: device.width*0.05,
+    },
+    wrapper: {
+        backgroundColor: '#000',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0
+    },
+    slide: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    photo: {
+        flex: 1,
+        width:device.width,
+        height:device.height,
+    },
+    text: {
+        color: '#fff',
+        fontSize: 30,
+        fontWeight: 'bold'
     },
 });
